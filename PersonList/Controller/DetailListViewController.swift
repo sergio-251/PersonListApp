550793180b1ded672b361c281dbe88e0ebf16a8d
EfimovSchool <sergio-251@yandex.ru>
2021-12-25 11:24:44 +0300

    Unselected

--- PersonList/Controller/DetailListViewController.swift
+++ PersonList/Controller/DetailListViewController.swift
@@ -60,5 +60,9 @@ class DetailListViewController: UITableViewController {
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
